e844c94babbae009589254f61e6677a44657267e
nagesh-merva <officialnagesh.merva@gmail.com>
2024-12-23 20:06:47 +0530

    basic setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Main from './pages/Main'
+import Aboutus from './pages/AboutUs'
+import Services from './pages/Services'
+import Contact from './pages/Contact'
 
 function App() {
 
-
   return (
     <>
-      <h1 className="font-raleway">hello world</h1>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/aboutus" element={<Aboutus />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   )
 }
